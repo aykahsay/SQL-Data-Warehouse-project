4c7e0b1d64ed8da00bb1b1f755242ccccca7f1b9
Ambachow Ykalom Kahsay <aykahsay19@gmail.com>
2025-11-03 20:22:11 +0300

    Add files via upload

---
+++ Scripts/Database and Schema/view DB tables and schemas.sql
@@ -0,0 +1,20 @@
+USE DataWarehouse;
+GO
+SELECT name 
+FROM sys.schemas;
+
+SELECT TABLE_SCHEMA, TABLE_NAME 
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_TYPE = 'BASE TABLE';
+
+
+
+SELECT name FROM sys.databases;
+
+
+EXEC silver.load_silver;
+
+
+
+SELECT *
+FROM silver.crm_cust_info
